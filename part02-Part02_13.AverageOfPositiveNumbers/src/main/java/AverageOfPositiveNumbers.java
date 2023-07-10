f63eb972682ee20b7764051fa56e5e7eb08dbd94
Kayode-Awe <kayode.awe@outlook.com>
2023-07-10 12:59:51 -0600

    complete part2

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -7,34 +7,30 @@ public class AverageOfPositiveNumbers {
         Scanner scanner = new Scanner(System.in);
 
         
-        double numOfNums = 0;
-        double sum = 0;
-        double positiveNums = 0;
+        int numOfNums = 0;
+        int sum = 0;
+        int positiveNums = 0;
         
         while(true) {
-            System.out.println("Give a number:");
-            double input = Double.valueOf(scanner.nextLine());
+
+            int input = Integer.valueOf(scanner.nextLine());
             
             if (input == 0){
                 break;
-            }
-            
-            
-            if(input > 0) {
-                
+
+            }else if(input > 0 ) {
                 positiveNums = positiveNums + 1;
                 numOfNums = numOfNums + 1;
                 sum = sum + input;
-            } else {
-                System.out.println("Cannot calculate the average");
+            } else if (input < 0) {
+                continue;
             }
-            
-            
-            
+        }
+
+        if (positiveNums == 0) {
+            System.out.println("Cannot calculate the average");
         }
         
-        System.out.println(sum/numOfNums);
-
-
+        System.out.println( 1.0 * sum/numOfNums);
     }
 }
--- part02-Part02_28.Word/src/main/java/Word.java
+++ part02-Part02_28.Word/src/main/java/Word.java
@@ -5,7 +5,12 @@ public class Word {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
 
+    }
+
+    public static String word() {
+        return "Success";
     }
 
 }
